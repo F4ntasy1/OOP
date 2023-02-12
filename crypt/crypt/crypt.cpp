fffc94d107879e7058908c77458ecb1c2c8d6de8
Dmitriy <dmitriypet03@bk.ru>
2023-02-12 22:59:18 +0300

    Добавил задание crypt по 1-й лабе

---
+++ crypt/crypt/crypt.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+void crypt(std::ifstream& input, std::ofstream& output, const int key)
+{
+    char ch;
+    while (input.get(ch))
+    {
+        int byte = ch ^ key;
+
+        int byteCrypt = 
+            ((byte << 2) & 0x1C) |
+            ((byte >> 5) & 0x03) |
+            ((byte << 3) & 0xC0) |
+            ((byte >> 2) & 0x20);
+
+        output << byteCrypt << ' ';
+    }
+}
+
+void decrypt(std::ifstream& input, std::ofstream& output, const int key)
+{
+    int byte;
+    while (input >> byte)
+    {
+        int byteDecrypt =
+            ((byte & 0x1C) >> 2) |
+            ((byte & 0x03) << 5) |
+            ((byte & 0xC0) >> 3) |
+            ((byte & 0x20) << 2);
+
+        char ch = byteDecrypt ^ key;
+
+        output << ch;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		std::cout << "Invalid arguments count\n"
+			<< "Usage: crypt.exe <action>(crypt|decrypt) "
+            << "<input file> <output file> <key>\n";
+		return 1;
+	}
+
+    std::ifstream input(argv[2]);
+    if (!input.is_open())
+    {
+        std::cout << "Failed to open " << argv[2] << " for reading\n";
+        return 1;
+    }
+
+    std::ofstream output(argv[3]);
+    if (!output.is_open())
+    {
+        std::cout << "Failed to open " << argv[3] << " for writing\n";
+        return 1;
+    }
+    
+    int key;
+    try 
+    {
+        key = std::stoi(argv[4]);
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << "Argument <key> must be numeric\n";
+        return 1;
+    }
+
+    if (key < 0 || key > 255)
+    {
+        std::cout << "Argument <key> must be in the range 0-255\n";
+        return 1;
+    }
+
+    std::string action = argv[1];
+
+    if (action == "crypt")
+    {
+        crypt(input, output, key);
+    }
+    else if (action == "decrypt")
+    {
+        decrypt(input, output, key);
+    }
+    else
+    {
+        std::cout << "Argument <action> is not defined\n"
+            << "Usage: crypt.exe <action>(crypt|decrypt) ...";
+        return 1;
+    }
+
+    if (!output.flush())
+    {
+        std::cout << "Failed to save data on disk\n";
+        return 1;
+    }
+
+    return 0;
+}
---
+++ crypt/crypt/test.bat
@@ -0,0 +1,48 @@
+@echo off
+
+set PROGRAM="%~1"
+
+rem проверяем программу на неверное количество аргументов на входе
+%PROGRAM% crypt
+if NOT ERRORLEVEL 1 goto err
+%PROGRAM% crypt test_data/hello-world.bin %TEMP%\out.txt 177 param
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем программу на неудачное открытие файла для чтения
+%PROGRAM% crypt non-existing-file %TEMP%\out.txt 177
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем программу на неудачное открытие файла для записи
+%PROGRAM% decrypt test_data/hello-world-crypt.bin %TEMP%\ 177
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем программу на некорректный параметр <action>
+%PROGRAM% non-exist-action test_data/hello-world.bin %TEMP%\out.txt 177
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем параметр <key> программы на граничные значения
+%PROGRAM% crypt test_data/hello-world.bin %TEMP%\out.txt -1
+if NOT ERRORLEVEL 1 goto err
+%PROGRAM% crypt test_data/hello-world.bin %TEMP%\out.txt 256
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем программу на некорректный параметр <key>
+%PROGRAM% crypt test_data/hello-world.bin %TEMP%\out.txt string
+if NOT ERRORLEVEL 1 goto err
+
+rem проверяем программу на шифрование
+%PROGRAM% crypt test_data/hello-world.bin %TEMP%\out.bin 177
+fc %TEMP%\out.bin test_data/hello-world-crypt.bin
+if ERRORLEVEL 1 goto err
+
+rem проверяем программу на дешифрование
+%PROGRAM% decrypt test_data/hello-world-crypt.bin %TEMP%\out.bin 177
+fc %TEMP%\out.bin test_data/hello-world.bin
+if ERRORLEVEL 1 goto err
+
+echo All tests passed
+exit /B 0
+
+:err
+echo Program testing failed
+exit /B 1
