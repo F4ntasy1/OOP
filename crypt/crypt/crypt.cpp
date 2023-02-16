2026da8e18721c6ccbea37db7b019947eaef3817
Dmitriy <dmitriypet03@bk.ru>
2023-02-16 17:22:46 +0300

    Исправлено задание crypt: добавлены exception-ы, создан метод validateParameters

--- crypt/crypt/crypt.cpp
+++ crypt/crypt/crypt.cpp
@@ -2,25 +2,21 @@
 #include <fstream>
 #include <string>
 
-/*ifstream / ofstream->istream / ostream
-вместо return 1/0 кидать exception в ф-ях
-разгрузить main - вынести функции
-передавать тяжелые объекты по ссылкам, изменять их и не возвращать, void
-*/
+const std::string ACTION_CRYPT = "crypt";
+const std::string ACTION_DECRYPT = "decrypt";
 
 void crypt(std::istream& input, std::ostream& output, const int key)
 {
     char ch;
-
     while (input.read((char*)&ch, sizeof(ch)))
     {
         int byte = ch ^ key;
 
         int byteCrypt = 
-            ((byte << 2) & 0x1C) |  //сдвиг на 2 влево, 0x1C = 00011100
-            ((byte >> 5) & 0x03) |  //сдвиг на 5 вправо, 0x03 = 00000011
-            ((byte << 3) & 0xC0) |  //сдвиг на 3 влево, 0xC0 = 11000000
-            ((byte >> 2) & 0x20);   //сдвиг на 2 вправо, 0x20 = 00100000
+            ((byte << 2) & 28) |    //28 = 00011100
+            ((byte >> 5) & 3) |     //3 = 00000011
+            ((byte << 3) & 192) |   //192 = 11000000
+            ((byte >> 2) & 32);     //32 = 00100000
 
         output << byteCrypt << ' ';
     }
@@ -29,14 +25,13 @@ void crypt(std::istream& input, std::ostream& output, const int key)
 void decrypt(std::istream& input, std::ostream& output, const int key)
 {
     int byte;
-
     while (input >> byte)
     {
         int byteDecrypt =
-            ((byte & 0x1C) >> 2) |
-            ((byte & 0x03) << 5) |
-            ((byte & 0xC0) >> 3) |
-            ((byte & 0x20) << 2);
+            ((byte & 28) >> 2) |
+            ((byte & 3) << 5) |
+            ((byte & 192) >> 3) |
+            ((byte & 32) << 2);
 
         char ch = byteDecrypt ^ key;
 
@@ -44,64 +39,100 @@ void decrypt(std::istream& input, std::ostream& output, const int key)
     }
 }
 
-int main(int argc, char* argv[])
+void validateParameters(
+    int argc, 
+    char* argv[], 
+    int& key, 
+    std::string& action,
+    std::ifstream& input,
+    std::ofstream& output)
 {
-    const int KEY_MAX = 255;
+    if (argc != 5)
+    {
+        throw std::invalid_argument("Invalid arguments count\n"
+            "Usage: crypt.exe <action>(crypt|decrypt) "
+            "<input file> <output file> <key>\n");
+    }
 
-	if (argc != 5)
-	{
-		std::cout << "Invalid arguments count\n"
-			<< "Usage: crypt.exe <action>(crypt|decrypt) "
-            << "<input file> <output file> <key>\n";
-		return 1;
-	}
+    action = argv[1];
+    if (action != ACTION_CRYPT && action != ACTION_DECRYPT)
+    {
+        throw std::invalid_argument("Argument <action> is not defined\n"
+            "Usage: crypt.exe <action>(crypt|decrypt) ...\n");
+    }
 
-    std::ifstream input(argv[2], std::ios::binary);
+    try
+    {
+        input.open(argv[2], std::ios::binary);
+    }
+    catch (const std::exception)
+    {
+        std::cout << "Failed to open " << argv[2] << " for reading\n";
+        throw std::invalid_argument("");
+    }
+
+    try
+    {
+        output.open(argv[3], std::ios::binary);
+    }
+    catch (const std::exception)
+    {
+        std::cout << "Failed to open " << argv[3] << " for writing\n";
+        throw std::invalid_argument("");
+    }
+
     if (!input.is_open())
     {
         std::cout << "Failed to open " << argv[2] << " for reading\n";
-        return 1;
+        throw std::invalid_argument("");
     }
 
-    std::ofstream output(argv[3], std::ios::binary);
     if (!output.is_open())
     {
         std::cout << "Failed to open " << argv[3] << " for writing\n";
-        return 1;
+        throw std::invalid_argument("");
     }
-    
-    int key;
-    try 
+
+    try
     {
         key = std::stoi(argv[4]);
     }
-    catch (const std::exception& /*ex*/)
+    catch (const std::exception)
     {
-        std::cout << "Argument <key> must be numeric\n";
+        throw std::invalid_argument("Argument <key> must be numeric\n");
+    }
+
+    if (key < 0 || key > std::numeric_limits<unsigned char>::max())
+    {
+        throw std::invalid_argument("Argument <key> must be in "
+            "the range 0 - 255\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::string action;
+    std::ifstream input;
+    std::ofstream output;
+    int key;
+
+    try
+    {
+        validateParameters(argc, argv, key, action, input, output);
+    }
+    catch (const std::invalid_argument& e)
+    {
+        std::cout << e.what();
         return 1;
     }
 
-    if (key < 0 || key > KEY_MAX)
-    {
-        std::cout << "Argument <key> must be in the range 0-255\n";
-        return 1;
-    }
-
-    std::string action = argv[1];
-
-    if (action == "crypt")
+    if (action == ACTION_CRYPT)
     {
         crypt(input, output, key);
     }
-    else if (action == "decrypt")
-    {
-        decrypt(input, output, key);
-    }
     else
     {
-        std::cout << "Argument <action> is not defined\n"
-            << "Usage: crypt.exe <action>(crypt|decrypt) ...\n";
-        return 1;
+        decrypt(input, output, key);
     }
 
     if (!output.flush())
